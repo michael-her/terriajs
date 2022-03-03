cd11b5d133c21588deb580b06caf9a47336134db
Sehui Min <dr.michael.her@gmail.com>
2022-03-03 13:31:08 +0900

    fix _previewInfo error

---
+++ .husky/husky.local.sh
@@ -0,0 +1,6 @@
+# Created by Husky v4.3.8 (https://github.com/typicode/husky#readme)
+#   At: 2022. 3. 3. 오후 1:09:45
+#   From: undefined (https://github.com/typicode/husky#readme)
+
+packageManager=yarn
+cd "."
